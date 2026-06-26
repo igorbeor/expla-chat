09c7a7ce2c0e78fc1b6608fd2156ee9e55e2aecd
Ihor Tretiak <igor.tretyak358@gmail.com>
2026-06-26 17:22:49 +0300

    feat: add users module/service

---
+++ apps/server/src/app/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { User, UserStatus } from '@chat/api-interfaces';
+
+@Injectable()
+export class UsersService {
+  private readonly users: Map<string, User> = new Map();
+
+  public initBots(bots: User[]): void {
+    for (const bot of bots) {
+      this.add(bot);
+    }
+  }
+
+  public add(user: User): void {
+    this.users.set(user.id, user);
+  }
+
+  public remove(id: string): void {
+    this.users.delete(id);
+  }
+
+  public get(id: string): User | undefined {
+    return this.users.get(id)
+  }
+
+  public getAll(): User[] {
+    return [...this.users.values()];
+  }
+
+  public updateStatus(id: string, status: UserStatus): void {
+    const user = this.users.get(id);
+    if (user) {
+      this.users.set(id, { ...user, status });
+    }
+  }
+}
